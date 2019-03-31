43480029bb6befcea4a26a0df477d48db1893944
Yannis THOMIAS <thomias.yannis@gmail.com>
2019-03-31 11:10:13 +0200

    First commit

---
+++ src/vendor.ts
@@ -0,0 +1,13 @@
+require('../webOSTVjs-1.1.0/webOSTV-dev.js');
+require('../webOSTVjs-1.1.0/webOSTV.js');
+
+// Angular
+import '@angular/animations';
+import '@angular/common';
+import '@angular/core';
+import '@angular/forms';
+import '@angular/http';
+import '@angular/router';
+
+// RxJS
+import 'rxjs';
